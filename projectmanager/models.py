4c0b4b1e62b7e44e0e6a139cb8708996528eab2b
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-09-08 11:14:37 +0200

    Add project and contributor models and migrations

--- projectmanager/models.py
+++ projectmanager/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    BACKEND = "BACKEND"
+    FRONTEND = "FRONTEND"
+    IOS = "IOS"
+    ANDROID = "ANDROID"
+
+    TYPE_CHOICES = (
+        (BACKEND, "Back-end"),
+        (FRONTEND, "Front-end"),
+        (IOS, "iOS"),
+        (ANDROID, "Android"),
+    )
+
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    project_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="authors",
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.author:
+            author_as_contributor = Contributor.objects.create(user=self.author)
+            author_as_contributor.project.add(self)
+            author_as_contributor.save()
+
+
+class Contributor(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="contributor",
+    )
+    project = models.ManyToManyField("Project", related_name="contributing")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
