8fc3e3e21bc50749cd8974f21e122e9cdc1ab3f2
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-09-26 22:48:23 +0200

    Fix issue description field saving and more
    -Show issues, comments and contributors if the user is a contributor of the project
    -Remove list view if there is no project or issue id filters

--- projectmanager/views.py
+++ projectmanager/views.py
@@ -21,6 +21,7 @@ from .permissions import (
     IssuePermission,
     CommentPermission,
     ContributorPermission,
+    is_contributor,
 )
 from .mixins import GetDetailSerializerMixin
 
@@ -68,19 +69,20 @@ class ContributorViewset(ModelViewSet):
     permission_classes = [IsAuthenticated, ContributorPermission]
 
     def get_queryset(self):
-        if self.action == "retrieve" or self.action == "update":
-            return Contributor.objects.all()
-
-        """ Verify the presence of ‘project_id’ in the url,
-        if yes apply filter to show contributors of the given project """
+        """Verify the presence of ‘project_id’ in the url,
+        if yes apply filter to show contributors of the given project"""
         project_id = self.request.GET.get("project_id")
-        if project_id is not None:
+        if project_id is not None and is_contributor(
+            self.request.user, get_object_or_404(Project, id=project_id)
+        ):
             queryset = Contributor.objects.filter(
                 project=get_object_or_404(Project, id=project_id)
             )
             return queryset
         else:
-            raise ValidationError(detail="apply a filter with project id.")
+            raise ValidationError(
+                detail="Apply a filter with project_id; a project that you are contributing to."
+            )
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
@@ -127,55 +129,73 @@ class ContributorViewset(ModelViewSet):
 
 
 class IssueViewset(GetDetailSerializerMixin, ModelViewSet):
+    """
+    Issue views endpoints.
+    Create/ update /delete: Author Contributor
+    Get list / details: Contributor
+    """
+
     serializer_class = IssueListSerializer
     detail_serializer_class = IssueDetailSerializer
 
     permission_classes = [IsAuthenticated, IssuePermission]
 
     def get_queryset(self):
-        if self.action == "retrieve" or self.action == "update":
-            return Issue.objects.all()
         """Verify the presence of ‘project_id’ in the url,
-        if yes apply filter to show issues of the given project"""
+        if yes and if the user is a contributor of this project
+        apply filter to show issues of the given project"""
         project_id = self.request.GET.get("project_id")
-        if project_id is not None:
+        if project_id is not None and is_contributor(
+            self.request.user, get_object_or_404(Project, id=project_id)
+        ):
             queryset = Issue.objects.filter(
                 project=get_object_or_404(Project, id=project_id)
             )
             return queryset
         else:
-            raise ValidationError(detail="apply a filter with project id.")
+            raise ValidationError(
+                detail="Apply a filter with project_id; a project that you are contributing to."
+            )
 
     @transaction.atomic
     def perform_create(self, serializer):
         serializer.save(
+            description=self.request.data["description"],
             author=get_object_or_404(
                 Contributor,
                 user=self.request.user,
                 project=self.request.data["project"],
-            )
+            ),
         )
 
 
 class CommentViewset(GetDetailSerializerMixin, ModelViewSet):
+    """
+    Comment views endpoints.
+    Create/ update /delete: Author Contributor
+    Get list / details: Contributor
+    """
+
     serializer_class = CommentListSerializer
-    """ detail_serializer_class = CommentDetailSerializer """
 
     permission_classes = [IsAuthenticated, CommentPermission]
 
     def get_queryset(self):
-        if self.action == "retrieve" or self.action == "update":
-            return Comment.objects.all()
         """Verify the presence of 'issue_id' in the url,
-        if yes apply filter to show the comments of the given issue"""
+        if yes and if the user is a contributor of the project in which the issue is
+        apply filter to show the comments of the given issue"""
         issue_id = self.request.GET.get("issue_id")
-        if issue_id is not None:
+        if issue_id is not None and is_contributor(
+            self.request.user, get_object_or_404(Issue, id=issue_id).project
+        ):
             queryset = Comment.objects.filter(
                 issue=get_object_or_404(Issue, id=issue_id)
             )
             return queryset
         else:
-            raise ValidationError(detail="apply a filter with issue id.")
+            raise ValidationError(
+                detail="Apply a filter with issue_id; an issue which is part of a project that you are contributing to."
+            )
 
     @transaction.atomic
     def perform_create(self, serializer):
